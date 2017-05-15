1be6b736f708cc97a816be90a857ccf1223bbe2c
chetan <chetan.c256@gmail.com>
2017-05-15 18:17:47 +0530

    Updated URLs for form processing @cc

--- scribe/myapp/urls.py
+++ scribe/myapp/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     url(r'myapp/auth/',views.authorize_user,name ='auth'),
     url(r'myapp/oauth2callback/',views.oauth2callback,name ='oauthcallback'),
     url(r'^myapp/$', views.index, name='index'),
-    url(r'^', views.sales_form)
+    url(r'^myapp/sales_form', views.get_sales_form),
+    url(r'^myapp/process_sales_form', views.process_sales_form),
+    url(r'^myapp/customer_form', views.get_customer_form),
+    url(r'^myapp/process_customer_form', views.process_customer_form)
 ]
